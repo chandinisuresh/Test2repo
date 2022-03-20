0de13c724fb4b1930f433ad0fcd01bf9e6979638
chandiniayaan <chandiniayaan955@gmail.com>
2022-03-19 23:12:21 -0400

    fifth commit

--- HelloWorld.java
+++ HelloWorld.java
@@ -2,13 +2,11 @@
 public class HelloWorld {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		
+		System.out.println("Hello");
 
-<<<<<<< HEAD
 		System.out.println("Hello");
-=======
-		System.out.println("Hello");
->>>>>>> 54bddcf131767f96bf3784ec4622e3854126afaf
+
 	}
 
 
